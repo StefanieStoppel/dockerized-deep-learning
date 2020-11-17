f9b7e19c2e1fd0211bca0c90481766ce9275922e
Stefanie Stoppel <sstoppel@inovex.de>
2020-11-17 17:43:04 +0100

    feat: add tabulate

--- src/solver.py
+++ src/solver.py
@@ -1,19 +1,11 @@
 import math
+from tabulate import tabulate
 
 
 class Solver:
 
     def demo(self, a, b, c):
-        d = b ** 2 - 4 * a * c
-        if d > 0:
-            disc = math.sqrt(d)
-            root1 = (-b + disc) / (2 * a)
-            root2 = (-b - disc) / (2 * a)
-            return root1, root2
-        elif d == 0:
-            return -b / (2 * a)
-        else:
-            return "This equation has no roots"
+        return a * b * c
 
 if __name__ == '__main__':
     solver = Solver()
@@ -23,4 +15,5 @@ while True:
     b = int(input("b: "))
     c = int(input("c: "))
     result = solver.demo(a, b, c)
-    print(result)
+    table = [("a", a), ("b", b), ("c", c), ("result", result)]
+    print(tabulate(table))
